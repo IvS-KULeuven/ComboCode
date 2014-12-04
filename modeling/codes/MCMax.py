832f4da31116f8fd3ea8178cc2a31902b66e6422
Robin Lombaert <robin.lombaert@ster.kuleuven.be>
2014-12-04 12:22:24 +0000

    Bugs: - Removing a reference to the model_id keyword in MCMax.readModelSpectrum

--- modeling/codes/MCMax.py
+++ modeling/codes/MCMax.py
@@ -57,7 +57,7 @@ def readModelSpectrum(dpath,rt_sed=1,fn_spec='spectrum45.0.dat'):
         else: raise IOError                                
     except IOError:
         print 'No spectrum was found or ray-tracing is off for ' + \
-              '%s. Taking average of theta-grid MCSpectra.'%model_id
+              'this model. Taking average of theta-grid MCSpectra.'
         dfiles = glob(dpath,'MCSpec*.dat')
         w = DataIO.readCols(filename=dfiles[0])[0]
         mcy_list = [DataIO.readCols(f)[1] for f in dfiles]
